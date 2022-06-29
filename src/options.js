a1e363fb9ae4493083c9e2235bc0d2cb80045018
weatherstar <weather@lovec.at>
2022-06-29 23:00:33 +0800

    style: format code

--- src/options.js
+++ src/options.js
@@ -1,43 +1,44 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-import { GITHUB_CONFIGS } from './constants';
+import { GITHUB_CONFIGS } from "./constants";
 
 let githubConfigsForm = null;
 
-
-function handleGithubConfigFormOnSubmit(e){
-    e.preventDefault();
-    let githubConfigs = {}
-    Array.from(githubConfigsForm.querySelectorAll('input[name]')).forEach(input => {
-        githubConfigs[input.name] = input.value;
-    })
-    chrome.storage.local.set(githubConfigs);
+function handleGithubConfigFormOnSubmit(e) {
+  e.preventDefault();
+  let githubConfigs = {};
+  Array.from(githubConfigsForm.querySelectorAll("input[name]")).forEach(
+    (input) => {
+      githubConfigs[input.name] = input.value;
+    }
+  );
+  chrome.storage.local.set(githubConfigs);
 }
 
 function bindEvents() {
-    githubConfigsForm.addEventListener('submit', handleGithubConfigFormOnSubmit);
+  githubConfigsForm.addEventListener("submit", handleGithubConfigFormOnSubmit);
 }
 
 function initElements() {
-    githubConfigsForm = document.querySelector('#github-configs')
+  githubConfigsForm = document.querySelector("#github-configs");
 }
 
-function init(){
-    initElements();
-    bindEvents();
-    setGithubConfigs();
+function init() {
+  initElements();
+  bindEvents();
+  setGithubConfigs();
 }
 
-function setGithubConfigs(){
-    chrome.storage.local.get(Object.values(GITHUB_CONFIGS), function(result){
-        Array.from(githubConfigsForm.querySelectorAll('input[name]')).forEach(input => {
-          input.value = result[input.name] || '';  
-        })
-    });
+function setGithubConfigs() {
+  chrome.storage.local.get(Object.values(GITHUB_CONFIGS), function (result) {
+    Array.from(githubConfigsForm.querySelectorAll("input[name]")).forEach(
+      (input) => {
+        input.value = result[input.name] || "";
+      }
+    );
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-   init()
+document.addEventListener("DOMContentLoaded", function () {
+  init();
 });
-
-
