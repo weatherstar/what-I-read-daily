ceb687a3b9b6b09345afe42b08e1331b0f092fbb
weatherstar <weather@onekey.so>
2022-09-29 09:46:56 +0800

    chore: test

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,44 +1,44 @@
-import { getFormattedToday } from './time'
+import { getFormattedToday } from "./time";
 
 export function buildReadingList(content, listItem) {
-    const today = getFormattedToday();
-  
-    try {
-      const contentWrapper = document.createElement("div");
-      contentWrapper.innerHTML = content;
-      const title = contentWrapper.querySelector("h1");
-      const section = contentWrapper.querySelector(`#reading-list-${today}`);
-  
-      if (!title) {
-        const title = document.createElement("h1");
-        title.innerText = "What I read daily";
-        contentWrapper.prepend(title);
-      }
-  
-      if (section) {
-        const list = section.querySelector("ul");
-        const listItemWrapper = document.createElement("div");
-        listItemWrapper.innerHTML = `<li>${listItem}</li>`;
-        list.prepend(listItemWrapper.firstElementChild);
-      } else {
-        const section = document.createElement("section");
-        section.id = `reading-list-${today}`;
-        const sectionTitle = document.createElement("h4");
-        sectionTitle.innerText = today;
-        const list = document.createElement("ul");
-        const listItemWrapper = document.createElement("div");
-        listItemWrapper.innerHTML = `<li>${listItem}</li>`;
-        list.append(listItemWrapper.firstElementChild);
-        section.append(sectionTitle);
-        section.append(list);
-        contentWrapper.insertBefore(
-          section,
-          contentWrapper.querySelector("h1").nextSibling
-        );
-      }
-  
-      return contentWrapper.innerHTML;
-    } catch (e) {
-      return content;
+  const today = getFormattedToday();
+
+  try {
+    const contentWrapper = document.createElement("div");
+    contentWrapper.innerHTML = content;
+    const title = contentWrapper.querySelector("h1");
+    const section = contentWrapper.querySelector(`#reading-list-${today}`);
+
+    if (!title) {
+      const title = document.createElement("h1");
+      title.innerText = "What I read daily";
+      contentWrapper.prepend(title);
     }
+
+    if (section) {
+      const list = section.querySelector("ul");
+      const listItemWrapper = document.createElement("div");
+      listItemWrapper.innerHTML = `<li>${listItem}</li>`;
+      list.prepend(listItemWrapper.firstElementChild);
+    } else {
+      const section = document.createElement("section");
+      section.id = `reading-list-${today}`;
+      const sectionTitle = document.createElement("h4");
+      sectionTitle.innerText = today;
+      const list = document.createElement("ul");
+      const listItemWrapper = document.createElement("div");
+      listItemWrapper.innerHTML = `<li>${listItem}</li>`;
+      list.append(listItemWrapper.firstElementChild);
+      section.append(sectionTitle);
+      section.append(list);
+      contentWrapper.insertBefore(
+        section,
+        contentWrapper.querySelector("h1").nextSibling
+      );
+    }
+
+    return contentWrapper.innerHTML;
+  } catch (e) {
+    return content;
   }
+}
